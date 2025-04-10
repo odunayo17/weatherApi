9f73770aff54583335af9cae1512a54f30635880
oduanayo <olufemioduanayo096@gmail.com>
2025-04-10 09:15:13 -0400

    `Update dotenv path in app.js`

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express           = require('express');
 const axios             = require('axios');
-const dotenv            = require('dotenv').config({ path: './config/.env' });
+const dotenv            = require('dotenv').config({ path: './.env' });
 const path              = require('path');
 
 const app       = express();
